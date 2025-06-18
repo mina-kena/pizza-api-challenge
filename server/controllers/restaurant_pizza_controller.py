a62c940b2a44a08c29a232da6b5e440a7d3c1e3b
Timina Makena <timina.makena@student.moringaschool.com>
2025-06-18 10:06:03 +0300

    add new changes

---
+++ server/controllers/restaurant_pizza_controller.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, jsonify, request
+from server.models.restaurant_pizza import RestaurantPizza
+from server.models.restaurant import Restaurant
+from server.models.pizza import Pizza
+from server import db
+
+restaurant_pizza_bp = Blueprint('restaurant_pizzas', __name__)
+
+@restaurant_pizza_bp.route('/restaurant_pizzas', methods=['POST'])
+def create_restaurant_pizza():
+    data = request.get_json()
+    
+    # Validate required fields
+    required_fields = ['price', 'pizza_id', 'restaurant_id']
+    if not all(field in data for field in required_fields):
+        return jsonify({"errors": ["Missing required fields"]}), 400
+    
+    # Check if restaurant and pizza exist
+    restaurant = Restaurant.query.get(data['restaurant_id'])
+    pizza = Pizza.query.get(data['pizza_id'])
+    
+    if not restaurant or not pizza:
+        return jsonify({"errors": ["Restaurant or Pizza not found"]}), 400
+    
+    # Validate price
+    if not 1 <= data['price'] <= 30:
+        return jsonify({"errors": ["Price must be between 1 and 30"]}), 400
+    
+    try:
+        restaurant_pizza = RestaurantPizza(
+            price=data['price'],
+            pizza_id=data['pizza_id'],
+            restaurant_id=data['restaurant_id']
+        )
+        
+        db.session.add(restaurant_pizza)
+        db.session.commit()
+        
+        return jsonify(restaurant_pizza.to_dict()), 201
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"errors": [str(e)]}), 400
