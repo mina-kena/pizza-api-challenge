12534f9bda614ed76e34c984513008c8d0fa9d71
Timina Makena <timina.makena@student.moringaschool.com>
2025-06-18 10:04:57 +0300

    add new changes

---
+++ server/controllers/restaurant_controller.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, jsonify, request
+from server.models.restaurant import Restaurant
+from server.models.restaurant_pizza import RestaurantPizza
+from server import db
+
+restaurant_bp = Blueprint('restaurants', __name__)
+
+@restaurant_bp.route('/restaurants', methods=['GET'])
+def get_restaurants():
+    restaurants = Restaurant.query.all()
+    return jsonify([restaurant.to_dict() for restaurant in restaurants])
+
+@restaurant_bp.route('/restaurants/<int:id>', methods=['GET'])
+def get_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if not restaurant:
+        return jsonify({"error": "Restaurant not found"}), 404
+    
+    # Get pizzas for this restaurant
+    restaurant_pizzas = RestaurantPizza.query.filter_by(restaurant_id=id).all()
+    pizzas = [rp.to_dict() for rp in restaurant_pizzas]
+    
+    response = restaurant.to_dict()
+    response['pizzas'] = pizzas
+    return jsonify(response)
+
+@restaurant_bp.route('/restaurants/<int:id>', methods=['DELETE'])
+def delete_restaurant(id):
+    restaurant = Restaurant.query.get(id)
+    if not restaurant:
+        return jsonify({"error": "Restaurant not found"}), 404
+    
+    db.session.delete(restaurant)
+    db.session.commit()
+    return '', 204
